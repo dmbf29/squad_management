b3e768bf6aa27e02c26108ae209b5d69a8512dd0
Douglas Berkley <douglasmberkley@gmail.com>
2023-02-06 15:32:55 +0900

    added some initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :teams, only: [:index]
+  resources :squads, only: [:show] do
+    collection do
+      post :import
+    end
+  end
 end
