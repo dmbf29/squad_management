5833f5d832e5a6d7e591e84ad485060d42482503
Douglas Berkley <douglasmberkley@gmail.com>
2023-02-07 15:07:09 +0900

    added header on show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :teams, only: [:index]
+  resources :teams, only: [:index] do
+    resources :squads, only: [:new]
+  end
   resources :squads, only: [:show] do
     member do
       get :import
