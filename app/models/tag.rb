856df86601bafda9b4d2661eb006a546676d8efa
Douglas Berkley <douglasmberkley@gmail.com>
2023-02-08 12:49:37 +0900

    added validation on tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -19,5 +19,7 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Tag < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+  validates :abbrev, presence: true, length: { minimum: 1, maximum: 3 }
+  validates :color, presence: true
 end
